eb29ce95a45600860a56a48a265da790554efd5e
noituri <yknomeh@protonmail.com>
2020-04-04 21:20:01 +0200

    Small lexer revamp

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module ExpectoLang
 
 go 1.12
-
-require github.com/bradleyjkemp/memviz v0.2.4-0.20190515150326-64c5f58aba8a
--- lexer.go
+++ lexer.go
@@ -1,72 +1,157 @@
+// GoLang Scanner inspired
 package main
 
 import (
 	"errors"
 	"strconv"
 	"unicode"
+	"unicode/utf8"
 )
+
+type Token int
 
 const (
-	TokEOF        = iota // End of string/file        0
-	TokFunction          // function				  1
-	TokEnd               // end of statements etc	  2
-	TokIdentifier        // Identifier				  3
-	TokReturn            // procedure return		  4
-	TokExtern            // extern procedure		  5
-	TokNumber            // number					  6
-	TokStr               // string					  7
-	TokBoolean           // boolean					  8
-	TokLParen            // (						  9
-	TokRParen            // )						  10
-	TokIf                // If						  11
-	TokElse              // If else					  12
-	TokElif              // if else if (elif)		  13
-	TokLoop              // for/while/foreach loop	  14
-	TokIn                // loop - element in array	  15
-	TokEqual             // ==						  16
-	TokAssign            // =						  17
-	TokAttribute         // #[attr1 = 0]			  18
-	TokAtom              // :atom					  19
-	TokUnknown           // Not specified type		  20
+	TokEOF Token = iota // End of string/file
+	TokIdentifier 		// Identifier
+	TokNumber     		// number
+	TokStr        		// string
+	TokLParen     		// (
+	TokRParen     		// )
+	TokLBrace			// {
+	TokRBrace			// }
+	TokEqual      		// ==
+	TokAssign     		// =
+	TokAttribute  		// #[attr1 = 0]
+	TokAtom       		// :atom
+
+	KWBegin
+	TokIn         		// loop - element in array
+	TokForLoop          // for/while/foreach loop
+	TokElse       		// If else
+	TokIf         		// If
+	TokFalse			// boolean false
+	TokTrue				// boolean true
+	TokExtern     		// extern procedure
+	TokUnknown    		// Not specified type
+	TokReturn     		// procedure return
+	TokFunction   		// function
+	KWEnd
 )
 
-type tokenType uint8
+var tokens = map[Token]string {
+	TokAtom: "ATOM",
+	TokUnknown: "UNKNOWN",
+	TokEOF: "EOF",
+	TokIdentifier: "IDENT",
+	TokNumber: "NUMBER",
+	TokStr: "STRING",
+	TokAttribute: "ATTRIBUTE",
+	TokExtern: "@",
+	TokFunction: "fun",
+	TokReturn: "return",
+	TokTrue: "true",
+	TokFalse: "false",
+	TokIf: "if",
+	TokElse: "else",
+	TokForLoop: "for",
+	TokIn: "in",
+	TokLParen: "(",
+	TokRParen: ")",
+	TokLBrace: "{",
+	TokRBrace: "}",
+	TokEqual: "==",
+	TokAssign: "=",
+}
 
-type Token struct {
-	kind tokenType
-	val  int
+var keywords map[string]Token
+
+func init() {
+	keywords = make(map[string]Token)
+	for i := KWBegin + 1; i < KWEnd; i++ {
+		keywords[tokens[i]] = i
+	}
+}
+
+func Lookup(identifier string) Token {
+	if tok, found := keywords[identifier]; found {
+		return tok
+	}
+	return TokIdentifier
 }
 
 type Lexer struct {
-	Source        string
-	CurrentToken  Token
-	Identifier    string
+	source        string
+	token	 	  Token
+	identifier    string
 	numVal        float64
 	strVal        string
-	CurrentChar   int
-	LastChar      uint8
+	offsetChar	  int
+	forwardOffset int
+	line		  int
+	col			  int
+	lastChar      rune
 	isEOF         bool
 	ignoreNewLine bool
 	ignoreSpace   bool
-	ignoreAtoms	  bool
-	IsFloat       bool
+	ignoreAtoms   bool
+	isFloat       bool
+}
+
+func (l Lexer) New(source string) Lexer {
+	lexer := Lexer{
+		source: source,
+		offsetChar: 0,
+		forwardOffset: 0,
+		line: 1,
+	}
+	_ = lexer.nextChar()
+	if lexer.lastChar == 0xFEFF {
+		_ = lexer.nextChar()
+	}
+
+	return lexer
 }
 
 func (l *Lexer) nextChar() error {
-	if l.CurrentChar+1 < len(l.Source) {
-		l.CurrentChar++
-		l.LastChar = l.Source[l.CurrentChar]
+	if l.forwardOffset < len(l.source) {
+		l.offsetChar = l.forwardOffset
+		if l.lastChar == '\n' {
+			l.col = 0
+			l.line++
+		}
+		l.col++
+		ch := rune(l.source[l.forwardOffset])
+		addOffset := 1
+		if ch == 0 {
+			return errors.New("null character")
+		} else if ch >= utf8.RuneSelf {
+			ch, addOffset = utf8.DecodeRune([]byte(l.source[l.forwardOffset:]))
+			if ch == utf8.RuneError && addOffset == 1 {
+				return errors.New("illegal UTF-8 encoding")
+			} else if ch == 0xFEFF && l.offsetChar > 0 {
+				return errors.New("0xFEFF is only allowed as a first character")
+			}
+		}
+
+		l.lastChar = ch
+		l.forwardOffset += addOffset
 		return nil
 	}
 
 	return errors.New("eof")
 }
 
+func (l *Lexer) peek() byte {
+	if l.forwardOffset < len(l.source) {
+		return l.source[l.forwardOffset]
+	}
+	return 0
+}
+
 func (l *Lexer) removeSpace() (stopLexing bool) {
-	for ((l.LastChar == 32 || l.LastChar == '\t') && l.ignoreSpace) || ((l.LastChar == 10 || l.LastChar == 13) && l.ignoreNewLine) {
+	for ((l.lastChar == 32 || l.lastChar == '\t') && l.ignoreSpace) || ((l.lastChar == 10 || l.lastChar == 13) && l.ignoreNewLine) {
 		if l.nextChar() != nil {
-			l.CurrentToken.kind = TokEOF
-			l.CurrentToken.val = -1
+			l.token = TokEOF
 			return true
 		}
 	}
@@ -75,113 +160,51 @@ func (l *Lexer) removeSpace() (stopLexing bool) {
 }
 
 func (l *Lexer) isAlphabetic() (stopLexing bool) {
-	if unicode.IsLetter(rune(l.LastChar)) {
-		l.Identifier = string(rune(l.LastChar))
+	if unicode.IsLetter(l.lastChar) {
+		l.identifier = string(l.lastChar)
 
-		if l.nextChar() != nil {
-			l.CurrentToken.kind = TokEOF
-			l.CurrentToken.val = -1
-			return true
-		}
 
-		for unicode.IsLetter(rune(l.LastChar)) || l.LastChar == '_' {
-			l.Identifier += string(rune(l.LastChar))
-
+		for unicode.IsLetter(l.lastChar) || l.lastChar == '_' {
 			if l.nextChar() != nil {
 				break
 			}
+			l.identifier += string(l.lastChar)
+
 		}
 
-		if l.Identifier == "fc" {
-			l.CurrentToken.kind = TokFunction
-			l.CurrentToken.val = -1
-			return true
-		}
-
-		if l.Identifier == "end" {
-			l.CurrentToken.kind = TokEnd
-			l.CurrentToken.val = -1
-			return true
-		}
-
-		if l.Identifier == "return" {
-			l.CurrentToken.kind = TokReturn
-			l.CurrentToken.val = -1
-			return true
-		}
-
-		if l.Identifier == "true" || l.Identifier == "false" {
-			l.CurrentToken.kind = TokBoolean
-			l.CurrentToken.val = -1
-			return true
-		}
-
-		if l.Identifier == "if" {
-			l.CurrentToken.kind = TokIf
-			l.CurrentToken.val = -1
-			return true
-		}
-
-		if l.Identifier == "else" {
-			l.CurrentToken.kind = TokElse
-			l.CurrentToken.val = -1
-			return true
-		}
-
-		if l.Identifier == "elif" {
-			l.CurrentToken.kind = TokElif
-			l.CurrentToken.val = -1
-			return true
-		}
-
-		if l.Identifier == "loop" {
-			l.CurrentToken.kind = TokLoop
-			l.CurrentToken.val = -1
-			return true
-		}
-
-		if l.Identifier == "in" {
-			l.CurrentToken.kind = TokIn
-			l.CurrentToken.val = -1
-			return true
-		}
-
-		l.CurrentToken.kind = TokIdentifier
-		l.CurrentToken.val = -1
+		l.token = Lookup(l.identifier)
 		return true
 	}
 	return false
 }
 
 func (l *Lexer) isDigit() (stopLexing bool) {
-	if unicode.IsNumber(rune(l.LastChar)) || l.LastChar == '.' {
+	if unicode.IsNumber(l.lastChar) || l.lastChar == '.' {
 		tempStr := ""
-		l.IsFloat = l.LastChar == '.'
+		l.isFloat = l.lastChar == '.'
 
-		for ; ; {
-			tempStr += string(rune(l.LastChar))
+		for {
+			tempStr += string(l.lastChar)
 			if l.nextChar() != nil {
-				l.CurrentToken.kind = TokEOF
-				l.CurrentToken.val = -1
+				l.token = TokEOF
 				return true
 			}
 
-			if l.LastChar == '.' {
-				if l.IsFloat {
+			if l.lastChar == '.' {
+				if l.isFloat {
 					panic("Invalid use of '.'")
 				} else {
-					l.IsFloat = true
+					l.isFloat = true
 				}
 			}
 
-			if !unicode.IsNumber(rune(l.LastChar)) && l.LastChar != 46 {
+			if !unicode.IsNumber(l.lastChar) && l.lastChar != 46 {
 				break
 			}
 		}
 
 		l.numVal, _ = strconv.ParseFloat(tempStr, 64)
-		l.CurrentToken.kind = TokNumber
-		l.CurrentToken.val = -1
+		l.token = TokNumber
 		return true
 	}
 
@@ -189,42 +212,39 @@ func (l *Lexer) isDigit() (stopLexing bool) {
 }
 
 func (l *Lexer) isComment() (stopLexing bool) {
-	if l.LastChar == '/' {
+	if l.lastChar == '/' {
 		l.isEOF = l.nextChar() != nil
-		if l.LastChar == '/' {
-			for ; ; {
+		ch := l.peek()
+		if ch == '/' {
+			for {
 				if l.nextChar() != nil {
-					l.CurrentToken.kind = TokEOF
-					l.CurrentToken.val = -1
+					l.token = TokEOF
 					return true
 				}
 
-				if l.LastChar == 13 || l.LastChar == 10 {
+				if l.lastChar == 13 || l.lastChar == 10 {
 					break
 				}
 			}
 
 			l.NextToken()
 			return true
-		} else if l.LastChar == '*' {
-			for ; ; {
+		} else if ch == '*' {
+			for {
 				if l.nextChar() != nil {
-					l.CurrentToken.kind = TokEOF
-					l.CurrentToken.val = -1
+					l.token = TokEOF
 					return true
 				}
 
-				if l.LastChar == '*' {
+				if l.lastChar == '*' {
 					if l.nextChar() != nil {
-						l.CurrentToken.kind = TokEOF
-						l.CurrentToken.val = -1
+						l.token = TokEOF
 						return true
 					}
 
-					if l.LastChar == '/' {
+					if l.lastChar == '/' {
 						if l.nextChar() != nil {
-							l.CurrentToken.kind = TokEOF
-							l.CurrentToken.val = -1
+							l.token = TokEOF
 							return true
 						}
 						break
@@ -234,26 +254,21 @@ func (l *Lexer) isComment() (stopLexing bool) {
 
 			l.NextToken()
 			return true
-		} else {
-			l.LastChar = '/'
-			return false
 		}
 	}
 	return false
 }
 
 func (l *Lexer) isParen() (stopLexing bool) {
-	if l.LastChar == '(' {
+	if l.lastChar == '(' {
 		l.isEOF = l.nextChar() != nil
-		l.CurrentToken.kind = TokLParen
-		l.CurrentToken.val = -1
+		l.token = TokLParen
 		return true
 	}
 
-	if l.LastChar == ')' {
+	if l.lastChar == ')' {
 		l.isEOF = l.nextChar() != nil
-		l.CurrentToken.kind = TokRParen
-		l.CurrentToken.val = -1
+		l.token = TokRParen
 		return true
 	}
 
@@ -261,11 +276,9 @@ func (l *Lexer) isParen() (stopLexing bool) {
 }
 
 func (l *Lexer) isExtern() (stopLexing bool) {
-	if l.LastChar == '@' {
+	if l.lastChar == '@' {
 		l.isEOF = l.nextChar() != nil
-
-		l.CurrentToken.kind = TokExtern
-		l.CurrentToken.val = -1
+		l.token = TokExtern
 		return true
 	}
 
@@ -273,27 +286,25 @@ func (l *Lexer) isExtern() (stopLexing bool) {
 }
 
 func (l *Lexer) isStr() (stopLexing bool) {
-	if l.LastChar == '"' {
+	if l.lastChar == '"' {
 		if l.nextChar() != nil {
-			l.CurrentToken.kind = TokEOF
-			l.CurrentToken.val = -1
+			l.token = TokEOF
 			return true
 		}
 
 		l.strVal = ""
-		for l.LastChar != '"' {
-			l.strVal += string(rune(l.LastChar))
-
+		for l.lastChar != '"' {
+			l.strVal += string(l.lastChar)
 			if l.nextChar() != nil {
 				l.isEOF = true
 				break
 			}
+
 		}
 
 		l.isEOF = l.nextChar() != nil
 
-		l.CurrentToken.kind = TokStr
-		l.CurrentToken.val = -1
+		l.token = TokStr
 		return true
 	}
 
@@ -301,95 +312,91 @@ func (l *Lexer) isStr() (stopLexing bool) {
 }
 
 func (l *Lexer) isEqual() (stopLexing bool) {
-	if l.LastChar == '=' {
+	if l.lastChar == '=' {
 		if l.nextChar() != nil {
-			l.CurrentToken.kind = TokEOF
-			l.CurrentToken.val = -1
+			l.token = TokEOF
 			return true
 		}
 
-		if l.LastChar != '=' {
-			l.CurrentToken.kind = TokAssign
-			l.CurrentToken.val = -1
+		if l.lastChar != '=' {
+			l.token = TokAssign
 			return true
 		}
 
 		l.isEOF = l.nextChar() != nil
-		l.CurrentToken.kind = TokEqual
-		l.CurrentToken.val = -1
+		l.token = TokEqual
 		return true
 	}
 
 	return false
 }
 
-func (l *Lexer) isAttribute() (stopLexing bool) {
-	if l.LastChar == '#' {
-		if l.nextChar() != nil {
-			l.CurrentToken.kind = TokEOF
-			l.CurrentToken.val = -1
-			return true
-		}
+//func (l *Lexer) isAttribute() (stopLexing bool) {
+//	if l.lastChar == '#' {
+//		if l.nextChar() != nil {
+//			l.CurrentToken.kind = TokEOF
+//			l.CurrentToken.val = -1
+//			return true
+//		}
+//
+//		if l.LastChar == '[' {
+//			l.isEOF = l.nextChar() != nil
+//			l.CurrentToken.kind = TokAttribute
+//			l.CurrentToken.val = -1
+//			return true
+//		}
+//	}
+//
+//	return false
+//}
 
-		if l.LastChar == '[' {
-			l.isEOF = l.nextChar() != nil
-			l.CurrentToken.kind = TokAttribute
-			l.CurrentToken.val = -1
-			return true
-		}
-	}
-
-	return false
-}
-
-func (l *Lexer) isAtom() (stopLexing bool) {
-	if l.LastChar == ':' {
-		oldToken := l.CurrentToken
-		oldChar := l.CurrentChar
-
-		l.ignoreSpace = false
-		l.ignoreNewLine = false
-
-		if l.nextChar() != nil {
-			l.CurrentToken.kind = TokEOF
-			l.CurrentToken.val = -1
-			l.ignoreSpace = true
-			l.ignoreNewLine = true
-			return true
-		}
-
-		atom := ""
-		for unicode.IsLetter(rune(l.LastChar)) || l.LastChar == '_' {
-			atom += string(rune(l.LastChar))
-
-			if l.nextChar() != nil {
-				break
-			}
-		}
-
-		l.ignoreSpace = true
-		l.ignoreNewLine = true
-
-		if atom == "" {
-			l.LastChar = ':'
-			l.CurrentToken = oldToken
-			l.CurrentChar = oldChar
-			return false
-		}
-
-		l.CurrentToken.kind = TokAtom
-		l.CurrentToken.val = -1
-		l.Identifier = ":" + atom
-		return true
-	}
-
-	return false
-}
+//func (l *Lexer) isAtom() (stopLexing bool) {
+//	if l.LastChar == ':' {
+//		oldToken := l.CurrentToken
+//		oldChar := l.CurrentChar
+//
+//		l.ignoreSpace = false
+//		l.ignoreNewLine = false
+//
+//		if l.nextChar() != nil {
+//			l.CurrentToken.kind = TokEOF
+//			l.CurrentToken.val = -1
+//			l.ignoreSpace = true
+//			l.ignoreNewLine = true
+//			return true
+//		}
+//
+//		atom := ""
+//		for unicode.IsLetter(rune(l.LastChar)) || l.LastChar == '_' {
+//			atom += string(rune(l.LastChar))
+//
+//			if l.nextChar() != nil {
+//				break
+//			}
+//		}
+//
+//		l.ignoreSpace = true
+//		l.ignoreNewLine = true
+//
+//		if atom == "" {
+//			l.LastChar = ':'
+//			l.CurrentToken = oldToken
+//			l.CurrentChar = oldChar
+//			return false
+//		}
+//
+//		l.CurrentToken.kind = TokAtom
+//		l.CurrentToken.val = -1
+//		l.Identifier = ":" + atom
+//		return true
+//	}
+//
+//	return false
+//}
 
 func (l *Lexer) NextToken() {
 	if l.isEOF {
-		l.CurrentToken.kind = TokEOF
-		l.CurrentToken.val = -1
+		l.token = TokEOF
 		return
 	}
 
@@ -397,9 +404,9 @@ func (l *Lexer) NextToken() {
 		return
 	}
 
-	if l.isAttribute() {
-		return
-	}
+	//if l.isAttribute() {
+	//	return
+	//}
 
 	if l.isAlphabetic() {
 		return
@@ -421,11 +428,11 @@ func (l *Lexer) NextToken() {
 		return
 	}
 
-	if !l.ignoreAtoms {
-		if l.isAtom() {
-			return
-		}
-	}
+	//if !l.ignoreAtoms {
+	//	if l.isAtom() {
+	//		return
+	//	}
+	//}
 
 	if l.isComment() {
 		return
@@ -435,10 +442,7 @@ func (l *Lexer) NextToken() {
 		return
 	}
 
-	tempChar := l.LastChar
-
 	l.isEOF = l.nextChar() != nil
 
-	l.CurrentToken.kind = TokUnknown
-	l.CurrentToken.val = int(tempChar)
+	l.token = TokUnknown
 }
